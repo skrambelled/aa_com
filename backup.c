73f85505f3fc7395e992821b1263720fd1fea828
skrambelled <skrambelled@gmail.com>
2021-08-06 11:07:16 -0700

    moved date func to XFUN

--- backup.c
+++ backup.c
@@ -1,31 +1,14 @@
 #pragma strict_types
 
 #include <daemons.h>
+#define XFUN "/w/maker/xfun/xfun"
 
 #define TP this_player()
 #define VALID_READ(X) "/secure/master"->valid_read(X,this_interactive()->query_real_name(),"")
 #define VALID_WRITE(X) "/secure/master"->valid_write(X,this_interactive()->query_real_name(),"")
 
-string my_month(string month_str) {
-  switch(month_str) {
-    case "Jan": return "01";
-    case "Feb": return "02";
-    case "Mar": return "03";
-    case "Apr": return "04";
-    case "May": return "05";
-    case "Jun": return "06";
-    case "Jul": return "07";
-    case "Aug": return "08";
-    case "Sep": return "09";
-    case "Oct": return "10";
-    case "Nov": return "11";
-    case "Dec": return "12";
-    default   : return "00";
-  }
-}
-
 int main(string str) { 
-  string file, newfile, year, month, day;
+  string file, newfile;
   
   seteuid(getuid());  
   
@@ -56,15 +39,6 @@ int main(string str) {
     write("Too large file: " +file+ "\n");
     return 1;
   }
-  
-  // collect our date info
-  year  = ctime()[20..<1];
-  month = my_month(ctime()[4..6]);
-  day   = ctime()[8..9];
-  
-  // account for whitespace in the day of the month int
-  if(day[0..0] == " ")
-    day[0..0] = "0";
     
   newfile = file;
   
@@ -72,7 +46,7 @@ int main(string str) {
   if(strlen(newfile) > 2 && newfile[<2..<1] == ".c")
     newfile = newfile[0..<3];
  
-  newfile = newfile + "." + year + month + day + "." +
+  newfile = newfile + "." + (string)XFUN->date_to_string() + "." +
             (string)TP->query_real_name();
   
   // clean up our backup file location          
